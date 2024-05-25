6dee716cd3aeaff55c387274fdef7bbd856bcd88
Juvito12 <jvsilva1221@outlook.com>
2024-05-25 19:47:41 -0300

    Atualização das condições

--- index.js
+++ index.js
@@ -21,9 +21,29 @@
     console.log(sim)
     console.log(nao)
 
-    if(sim == 2){
-        alert("Mínimo inferior Seu resultado apresenta poucos índices de predisposição para diabetes. Entretanto, é sempre bom ficar atento e fazer exames de rotina anualmente.")
+    const nome = document.getElementById("finish")
+    
+
+    if(sim > 0){
+      nome.innerHTML = "<p> <b>Mínimo inferior</b>. Seu resultado apresenta poucos índices de predisposição para diabetes. Entretanto, é sempre bom ficar atento e fazer exames de rotina anualmente.</p>"
+    } 
+    
+    if(sim > 2){
+        nome.innerHTML = "<p> <b>Baixo risco</b>. Seu resultado apresenta pequenos índices de predisposição de diabetes, e neste caso, podem ser causados por outros fatores psicológicos ou fisiológicos. Fique atento para caso apareça ou perceba outros sintomas e faça exames de rotina anualmente.</p>"
     }
+
+    if(sim > 4){
+        nome.innerHTML = "<p> <b>Médio Risco</b>. Seu resultado apresenta índices significativos de predisposição de diabetes, por isso, é importante ficar atento aos fatores de risco e procurar um atendimento profissional para realização de exames de rotina e um possível diagnostico. Lembre-se de que manter hábitos saudáveis é fundamental para o controle dos sintomas. <strong>Consulte seu médico ou procure uma unidade de saúde em sua regional. O quanto antes notamos uma predisposição, mais simples são os cuidados e intervenções de controle e prevenção.</strong> </p>"
+    }
+
+    if(sim > 6){
+        nome.innerHTML = "<p> <b>Alto Risco!</b> Seu resultado apresenta alto índice de sintomas de diabetes. <strong>É aconselhável consultar o seu médico ou procurar atendimento em uma unidade de saúde perto de você</strong>. Esse é o caminho para obter um diagnóstico seguro e receber as recomendações necessárias para o seu quadro clínico. <strong>Fique atento com sua saúde. Lembre-se que manter hábitos saudáveis é fundamental para prevenir o diabetes.</strong> </p>"
+    }
+
+   
+    
+
+
 
 
     
